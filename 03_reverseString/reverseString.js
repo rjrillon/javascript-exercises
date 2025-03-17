7f11ef9567e5c314aab2fcbdd86c220600610405
Robin <rjrillon97@gmail.com>
2025-03-17 14:07:34 -0700

    exercise 03 done
    used string functions and regex to parse input and
    pass tests including spaces and special characters.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+// const reverseString = function (str) {
+//   let reversedString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedString += str[i];
+//   }
+//   return reversedString;
+// };
 
+const reverseString = function (str) {
+  str.replace(/[^A-Za-z0-9]/g, "");
+
+  return str.split("").reverse().join("");
 };
 
+//reverseString("word");
 // Do not edit below this line
 module.exports = reverseString;
