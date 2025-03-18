74204d980eeddf71033231947c2f03690d45a108
Robin <rjrillon97@gmail.com>
2025-03-17 21:58:05 -0700

    exercise 05 done
    created a function to add sum of all numbers including
    those in-between the given inputs. also handled test
    cases where given inputs were not acceptable and learned
    of a few functions to help test that.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let start = num1;
+  let end = num2;
+  let result = 0;
 
+  if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  if (start > end) {
+    start = num2;
+    end = num1;
+  }
+
+  //loop from start to end values
+  while (start <= end) {
+    result += start;
+    start++;
+  }
+  return result;
 };
+
+// console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
