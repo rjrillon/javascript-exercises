8658dc62c127dbdae395d33d0b4f203de5ce4764
Robin <rjrillon97@gmail.com>
2025-03-17 23:00:11 -0700

    exercise 06 done
    in this exercise there's pretty clear test conditions.
    however, the order in which we check each condition
    matters so that's what makes it confusing..

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // If the year is divisible by 400, it is a leap year
+  if (year % 400 === 0) {
+    return true;
+  }
+  // If the year is divisible by 100 (but not by 400), it is not a leap year
+  if (year % 100 === 0) {
+    return false;
+  }
+  // If the year is divisible by 4 (but not by 100), it is a leap year
+  if (year % 4 === 0) {
+    return true;
+  }
+  // Otherwise, it is not a leap year
+  return false;
 };
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
