2efaf7ed4d7dc0f456ff51fffae3b6c2f363e859
Robin <rjrillon97@gmail.com>
2025-03-17 18:39:46 -0700

    exercise 04 done
    made a function to remove multiple values from an array.
    used an array function .filter() to check if each item
    should be passed to the new array. so in the value
    passed to .filter() is a callback function to iterate
    through the given array and return true if it doesnt
    match with all the target values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, target) {
+//   let newArray = [];
+
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] !== target) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
+// };
+// console.log(removeFromArray([1, 2, 3, 4], 3));
+
+const removeFromArray = function (arr, ...targets) {
+  const newArray = arr.filter(arrItem => !targets.includes(arrItem));
+  return newArray;
 };
 
 // Do not edit below this line
