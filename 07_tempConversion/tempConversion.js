8dacebec94a2464b660e197a84d28882833d8524
Robin <rjrillon97@gmail.com>
2025-03-17 23:19:07 -0700

    exercise 07 done
    simple conversion functions. looked up their respective
    formulas and then handled test conditions by adding
    rounding to nearest tenth.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  let convertedNum = ((num - 32) * 5) / 9;
+  result = Math.round(convertedNum * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  let convertedNum = (num * 9) / 5 + 32;
+  result = Math.round(convertedNum * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
