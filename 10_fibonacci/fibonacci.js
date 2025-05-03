0d92c7d75e71a5af29ebe02f405fbdff4430cf15
Robin <rjrillon97@gmail.com>
2025-05-03 10:48:13 -0700

    finished 10 fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num === "string") {
+    num = parseInt(num);
+  }
 
+  // Handle the specific case for "0"
+  if (num === 0) {
+    return 0;
+  }
+
+  if (num <= 0) {
+    return "OOPS";
+  }
+
+  if (num <= 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
