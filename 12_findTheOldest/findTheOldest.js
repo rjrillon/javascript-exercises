931a169d0293d92699ef7707fc4ecf01638c68d8
Robin <rjrillon97@gmail.com>
2025-05-03 14:01:43 -0700

    Finished 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrOfObj) {
+  let sorted = arrOfObj.sort((obj1, obj2) => {
+    let currDate = new Date().getFullYear();
 
+    let age1 = (obj1.yearOfDeath || currDate) - obj1.yearOfBirth;
+    let age2 = (obj2.yearOfDeath || currDate) - obj2.yearOfBirth;
+    return age2 - age1;
+  });
+  return sorted[0];
 };
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ];
+// console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
