ded3a919a45a6e8c262692e06e921708f6c9f414
Robin <rjrillon97@gmail.com>
2025-05-02 20:42:14 -0700

    finished 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.toLowerCase();
+  str = str.replace(/[^A-Za-z0-9]/g, ""); //regex
 
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 };
 
 // Do not edit below this line
